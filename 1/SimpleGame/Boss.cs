c88b8edf56c544c08d963af1f8c2653c639abb6d
Nikita <kurnosov.n.a.15@gmail.com>
2025-03-28 17:29:24 +0300

    My first commit for LAMOP

---
+++ 1/SimpleGame/Boss.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SimpleGame
+{
+    internal class Boss
+    {
+        private float _health;
+        private float _damage;
+        private float _fireAttackMultiplier = 1.4f;
+        private float _iceBurpMultiplier = 1.3f;
+
+        public Boss(float health, float damage)
+        {
+            _health = health;   
+            _damage = damage;
+        }
+
+        public void ApplyDamage(float damage)
+        {
+            _health -= damage;
+            if (_health < 0) { Console.WriteLine("VSE CONETS BOSSU"); };
+        }
+
+        public void PawStrike(Hero hero)
+        {
+            hero.ApplyDamage(_damage);
+        }
+
+        public void FireAttack(Hero hero)
+        {
+            hero.ApplyDamage(_damage * _fireAttackMultiplier);
+        }
+
+        public void IceBurp(Hero hero)
+        {
+            hero.ApplyDamage(_damage * _iceBurpMultiplier);
+        }
+    }
+}
---
+++ 1/SimpleGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SimpleGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Hero(150, 150, 20);
+            Boss boss = new Boss(1000, 13);
+
+            Console.WriteLine($"ВЫ ПОПАЛИ В ИГРУ, ВАМ НУЖНО ПОБЕДИТЬ БОССА, У НЕГО {boss.}");
+        }
+    }   
+}
