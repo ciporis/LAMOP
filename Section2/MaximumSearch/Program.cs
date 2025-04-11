33f68ce06e51fad5ee371940ad6815a4ce1d24c7
Nikita <kurnosov.n.a.15@gmail.com>
2025-04-11 09:04:15 +0300

    Finished MaxSearch

---
+++ Section2/MaximumSearch/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaximumSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[10,10];
+
+            Random random = new Random();
+            int rightRandomBorder = 100;
+
+            List<int> matrixElements = new List<int>(); 
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(rightRandomBorder);
+                    matrixElements.Add(matrix[i, j]);
+                    Console.Write($"{matrix[i, j]} ");
+                }                
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            int maxMatrixElement = matrixElements.Max();
+
+            Console.WriteLine($"Максимальный элемент: {maxMatrixElement}");
+            Console.WriteLine();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if(matrix[i, j] == maxMatrixElement)
+                        matrix[i, j] = 0;
+                    
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
