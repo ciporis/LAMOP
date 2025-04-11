0ff4a1c18632dde695618a69d5333b94b1555374
Nikita <kurnosov.n.a.15@gmail.com>
2025-04-11 10:47:48 +0300

    Finished DynamicArray

---
+++ Section2/DynamicArray/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace DynamicArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] userNumbers = new int[0];
+
+            const string StopWord = "exit";
+            const string SumCommand = "sum";
+
+            bool isWorking = true;
+
+            while (isWorking)
+            {
+                Console.WriteLine("Введите число/sum/exit");
+                string userInput = Console.ReadLine();
+
+                if (userInput == StopWord)
+                {
+                    isWorking = false;
+                    break;
+                }
+                    
+
+                if (userInput == SumCommand)
+                {
+                    Console.WriteLine(userNumbers.Sum());
+                }
+                else
+                {
+                    userNumbers = ArrayAppend(userNumbers, Convert.ToInt32(userInput));
+                    for (int i = 0; i < userNumbers.Length; i++)
+                    {
+                        Console.Write($"{userNumbers[i]} ");
+                    }
+                }
+            }
+        }
+
+        private static int[] ArrayAppend(int[] array, int number)
+        {
+            int[] resizedArray = new int[array.Length + 1];
+
+            for (int i = 0; i < array.Length; i++)
+                resizedArray[i] = array[i];
+
+            resizedArray[resizedArray.Length - 1] = number;
+
+            return resizedArray;
+        }
+    }
+}
