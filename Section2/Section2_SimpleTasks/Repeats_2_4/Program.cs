8c4be200d8689e2b11a872ce75cb7e11e9ba12f9
Nikita <kurnosov.n.a.15@gmail.com>
2025-04-30 13:47:49 +0300

    JDKf

--- Section2/Section2_SimpleTasks/Repeats_2_4/Program.cs
+++ Section2/Section2_SimpleTasks/Repeats_2_4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
 
 namespace Repeats_2_4
 {
@@ -6,10 +6,41 @@ namespace Repeats_2_4
     {
         static void Main(string[] args)
         {
+            int arrayLength = 30;
             int leftRandomBorder = 0;
             int rightRandomBorder = 100;
 
-            int randomNumber = InclusiveRandom.InclusiveNext();
+            int[] array = new int[arrayLength];
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                array[i] = InclusiveRandom.InclusiveNext(leftRandomBorder, rightRandomBorder);
+            }
+
+            int maxRepeatsCount = 0;
+            int repeatsCount = 0;
+            int repeatingNumber = 0;
+
+            for (int i = 0; i < arrayLength - 1; i++)
+            {
+                if (array[i] == array[i + 1])
+                {
+                    repeatsCount++;
+                    
+                    if(repeatsCount > maxRepeatsCount)
+                    {
+                        maxRepeatsCount = repeatsCount + 1;
+                        repeatingNumber = array[i];
+                    }
+                }
+                else
+                {
+                    repeatsCount = 0;
+                }
+            }
+
+            Console.WriteLine($"Array: {string.Join(" ", array)}");
+            Console.WriteLine($"Number: {repeatingNumber}\nMax repeats: {maxRepeatsCount}");
         }
 
         public static class InclusiveRandom
